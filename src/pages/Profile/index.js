3d3883fc360ab235074ca819a60b456ce1e8f758
Gustavo Dolzan <gudolzan@gmail.com>
2021-11-09 17:36:41 -0300

    Fix local storage issues

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import storage from '../../services/storage';
 
 function Profile() {
-  const userEmail = storage.read('user');
+  const { email } = JSON.parse(localStorage.getItem('user'));
 
   return (
     <div>
       <h1>Profile</h1>
-      <h2 data-testid="profile-email">{ userEmail }</h2>
-      <Link to="/receitas-feitas" data-testid="profile-done-btn">Receitas Feitas</Link>
-      <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">Receitas Favoritas</Link>
-        <Link to="/" onClick={ storage.clear('user') }data-testid="profile-logout-btn">Sair</Link>
+      <h2 data-testid="profile-email">{email}</h2>
+      <Link data-testid="profile-done-btn" to="/receitas-feitas">
+        Receitas Feitas
+      </Link>
+      <Link data-testid="profile-favorite-btn" to="/receitas-favoritas">
+        Receitas Favoritas
+      </Link>
+      <Link
+        data-testid="profile-logout-btn"
+        onClick={ () => localStorage.clear() }
+        to="/"
+      >
+        Sair
+      </Link>
     </div>
   );
 }
