a390c0ee4d3eaca16aecffc94025b4c1ab1e8117
PeDD1 <pedrohr0909@gmail.com>
2021-11-09 16:43:36 -0300

    Profile page w/ links and email setted

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import storage from '../../services/storage';
 
 function Profile() {
+  const userEmail = storage.read('user')
+
   return (
     <div>
       <h1>Profile</h1>
+      <h2>{ userEmail }</h2>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      </Link>
+      <Link to="/">
+        <button type="button" onClick={ storage.clear('user') }>Sair</button>
+      </Link>
     </div>
   );
 }
