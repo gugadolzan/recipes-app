407a39616ea8078330005cb6b69226e3d19fa189
Gustavo Dolzan <gudolzan@gmail.com>
2021-11-09 19:39:30 -0300

    Setup Context API

---
+++ src/context/RecipesProvider.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import RecipesContext from './RecipesContext';
+
+const { Provider } = RecipesContext;
+
+function RecipesProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+
+  const value = {
+    loading,
+    setLoading,
+  };
+
+  return <Provider value={ value }>{children}</Provider>;
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default RecipesProvider;
