b861262ffe7af4f2364e82d400920c27f347683e
PeDD1 <pedrohr0909@gmail.com>
2021-11-09 17:11:25 -0300

    Data test id added an rmv btns

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,21 +3,15 @@ import { Link } from 'react-router-dom';
 import storage from '../../services/storage';
 
 function Profile() {
-  const userEmail = storage.read('user')
+  const userEmail = storage.read('user');
 
   return (
     <div>
       <h1>Profile</h1>
-      <h2>{ userEmail }</h2>
-      <Link to="/receitas-feitas">
-        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-      </Link>
-      <Link to="/receitas-favoritas">
-        <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      </Link>
-      <Link to="/">
-        <button type="button" onClick={ storage.clear('user') }>Sair</button>
-      </Link>
+      <h2 data-testid="profile-email">{ userEmail }</h2>
+      <Link to="/receitas-feitas" data-testid="profile-done-btn">Receitas Feitas</Link>
+      <Link to="/receitas-favoritas" data-testid="profile-favorite-btn">Receitas Favoritas</Link>
+        <Link to="/" onClick={ storage.clear('user') }data-testid="profile-logout-btn">Sair</Link>
     </div>
   );
 }
