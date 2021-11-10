6eaf2a70334838d1ddfffdd9a6c39037679a7e07
Gustavo Dolzan <gudolzan@gmail.com>
2021-11-10 14:22:12 -0300

    Update styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import './Profile.css';
+
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
   const email = user ? user.email : null;
@@ -11,20 +13,31 @@ function Profile() {
   return (
     <>
       <Header />
-      <h2 data-testid="profile-email">{ email }</h2>
-      <Link data-testid="profile-done-btn" to="/receitas-feitas">
-        Receitas Feitas
-      </Link>
-      <Link data-testid="profile-favorite-btn" to="/receitas-favoritas">
-        Receitas Favoritas
-      </Link>
-      <Link
-        data-testid="profile-logout-btn"
-        onClick={ () => localStorage.clear() }
-        to="/"
-      >
-        Sair
-      </Link>
+      <div className="profile-page">
+        <h2 data-testid="profile-email">{email}</h2>
+        <Link
+          className="profile-btn"
+          data-testid="profile-done-btn"
+          to="/receitas-feitas"
+        >
+          Receitas Feitas
+        </Link>
+        <Link
+          className="profile-btn"
+          data-testid="profile-favorite-btn"
+          to="/receitas-favoritas"
+        >
+          Receitas Favoritas
+        </Link>
+        <Link
+          className="profile-btn"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+          to="/"
+        >
+          Sair
+        </Link>
+      </div>
       <Footer />
     </>
   );
