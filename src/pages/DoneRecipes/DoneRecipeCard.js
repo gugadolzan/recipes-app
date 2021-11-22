65b6cf384f6a91459e0d64867efec46863d709c8
Gustavo Dolzan <gudolzan@gmail.com>
2021-11-22 03:42:09 -0300

    Done recipes page

---
+++ src/pages/DoneRecipes/DoneRecipeCard.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import copy from 'clipboard-copy';
+import shareIcon from '../../images/shareIcon.svg';
+
+function DoneRecipeCard({ index, recipe }) {
+  const [showCopyToClipboard, setShowCopyToClipboard] = useState(false);
+  const {
+    alcoholicOrNot,
+    area,
+    category,
+    doneDate,
+    id,
+    image,
+    name,
+    tags,
+    type,
+  } = recipe;
+  const recipeDetailsLink = type === 'comida' ? `/comidas/${id}` : `/bebidas/${id}`;
+
+  return (
+    <div className="done-recipe-card">
+      <Link to={ recipeDetailsLink }>
+        <img
+          className="done-recipes-card-image"
+          data-testid={ `${index}-horizontal-image` }
+          src={ image }
+          alt={ name }
+        />
+      </Link>
+      <div className="done-recipe-card-info">
+        <p
+          className="done-recipe-card-top-text"
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          {type === 'comida' ? `${area} - ${category}` : alcoholicOrNot}
+        </p>
+        <div className="done-recipe-card-share">
+          <input
+            alt="Share button"
+            data-testid={ `${index}-horizontal-share-btn` }
+            onClick={ () => {
+              setShowCopyToClipboard(true);
+              copy(
+                window.location.href.replace(
+                  '/receitas-feitas',
+                  `${recipeDetailsLink}`,
+                ),
+              );
+            } }
+            src={ shareIcon }
+            type="image"
+          />
+          {showCopyToClipboard && <span>Link copiado!</span>}
+        </div>
+        <Link
+          className="done-recipe-card-name"
+          data-testid={ `${index}-horizontal-name` }
+          to={ recipeDetailsLink }
+        >
+          {name}
+        </Link>
+        <p
+          className="done-recipe-card-done-date"
+          data-testid={ `${index}-horizontal-done-date` }
+        >
+          {`Done in: ${doneDate}`}
+        </p>
+        {type === 'comida' && (
+          <div className="done-recipe-card-tags-container">
+            {tags.map((tagName, tagIndex) => (
+              <span
+                className="done-recipe-card-tag"
+                data-testid={ `${index}-${tagName}-horizontal-tag` }
+                key={ tagIndex }
+              >
+                {tagName}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+DoneRecipeCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  recipe: PropTypes.shape({
+    alcoholicOrNot: PropTypes.string.isRequired,
+    area: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    doneDate: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default DoneRecipeCard;
--- src/pages/DoneRecipes/index.js
+++ src/pages/DoneRecipes/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import copy from 'clipboard-copy';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
+import DoneRecipeCard from './DoneRecipeCard';
 
 import './DoneRecipes.css';
 
@@ -8,32 +8,32 @@ const FILTER_BUTTONS = [
   {
     dataTestId: 'filter-by-all-btn',
     text: 'All',
-    value: 'All',
+    value: '',
   },
   {
     dataTestId: 'filter-by-food-btn',
     text: 'Food',
-    value: 'Meals',
+    value: 'comida',
   },
   {
     dataTestId: 'filter-by-drink-btn',
     text: 'Drinks',
-    value: 'Drinks',
+    value: 'bebida',
   },
 ];
 
 function DoneRecipes() {
-  const [filter, setFilter] = useState('All');
-  const [loading, setLoading] = useState(false);
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  // React.useEffect(() => {
-  //   const cocktails = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  //   return () => {
+  useEffect(() => {
+    const doneRecipesStorage = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    setDoneRecipes(doneRecipesStorage);
+  }, []);
 
-  //   };
-  // }, []);
-
-  console.log(filter);
+  if (doneRecipes.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -53,41 +53,13 @@ function DoneRecipes() {
             </button>
           ))}
         </div>
-        <img
-          className="done-recipes-image"
-          data-testid="0-horizontal-image"
-          src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
-          alt=""
-        />
-        <p data-testid="0-horizontal-top-text">Italian - Vegetarian</p>
-        <p data-testid="0-horizontal-name">Spicy Arrabiata Penne</p>
-        <div data-testid="0-horizontal-done-date">Texto da data da receita</div>
-        <button
-          type="button"
-          data-testid="0-horizontal-share-btn"
-          src="/images/shareIcon.svg"
-          onClick={ async () => {
-            setLoading(true);
-            copy('http://localhost:3000/comidas/52771');
-            copy(window.location.href);
-          } }
-        >
-          share
-        </button>
-        {loading && <p>Link copiado!</p>}
-        <div data-testid="0-Pasta-horizontal-tag"> Pasta</div>
-        <div data-testid="0-Curry-horizontal-tag">Curry</div>
-        <p
-          data-testid="1-horizontal-image"
-          src="https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg"
-        >
-          Link copiado
-        </p>
-        <div data-testid="1-horizontal-done-date"> 23/06/2020</div>
-        <div data-testid="1-horizontal-top-text">Alcoholic</div>
-        <div data-testid="1-horizontal-name">Aquamarine</div>
-        <div data-testid="1-horizontal-share-btn" src="images/shareIcon.svg" />
-        <div data-testid="0-horizontal-done-date"> 23/06/2020</div>
+        <div className="done-recipes-container">
+          {doneRecipes
+            .filter(({ type }) => type.includes(filter))
+            .map((recipe, index) => (
+              <DoneRecipeCard key={ index } index={ index } recipe={ recipe } />
+            ))}
+        </div>
       </div>
     </>
   );
