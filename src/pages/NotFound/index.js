7c291606e5cde2a247683b4e77940681167f9936
Gustavo Dolzan <gudolzan@gmail.com>
2021-11-28 01:37:14 -0300

    Create NotFound page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <div className="header-footer-padding main-background">
+      <div className="not-found">
+        <p>Page not found!</p>
+        <div>
+          <Link to="/comidas">Return</Link>
+          <span> to main page.</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
